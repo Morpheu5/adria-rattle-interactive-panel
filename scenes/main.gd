349b016658dd1b9e029e87cb2cdfa8689c697530
Andrea Franceschini <andrea.franceschini@gmail.com>
2025-06-03 15:38:10 +0200

    :rocket: This *should* be ready to launch shortly -- minus the NFC thing which is now simulated with an [ENTER] keystroke

--- scenes/main.gd
+++ scenes/main.gd
@@ -8,7 +8,8 @@ var input_handling = true
 var labels = []
 
 var menu_visible = true
-var interactable = true
+var interactable = false
+var info_screen_open = false
 
 var info_screen_scene = preload("res://scenes/InfoScreen.tscn")
 var current_info_screen
@@ -23,6 +24,9 @@ func _ready() -> void:
 	]
 	$MainMenu/Labels/VBoxContainer.modulate = Color(1.0, 1.0, 1.0, 1.0)
 	$MainMenu/Labels/LanguageButton.get_popup().id_pressed.connect(_on_language_pressed)
+	menu_visible = true
+	interactable = false
+	$SceneRoot/Camera3D.position = Vector3(-1.2, 0.5, 6)
 
 func is_distance_shorter(label, event, _min):
 	return label.get_screen_position().distance_to(event.position) < _min.get_screen_position().distance_to(event.position)
@@ -36,24 +40,25 @@ func load_info_screen(filename: String) -> void:
 		$MainMenu.add_child(current_info_screen)
 		create_tween().tween_property(current_info_screen, "modulate", Color(1.0, 1.0, 1.0, 1.0), 0.25)
 		interactable = false
+		info_screen_open = true
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if (event is InputEventScreenTouch or event is InputEventMouseButton) and not event.is_pressed() and menu_visible and interactable:
-		var closest = labels.reduce(func(_min, label): return label if is_distance_shorter(label, event, _min) else _min)
-		var i = labels.find(closest)
-		load_info_screen(labels[i].filename)
+	#if (event is InputEventScreenTouch or event is InputEventMouseButton) and not event.is_pressed() and menu_visible and interactable:
+		#var closest = labels.reduce(func(_min, label): return label if is_distance_shorter(label, event, _min) else _min)
+		#var i = labels.find(closest)
+		#load_info_screen(labels[i].filename)
 	if (event is InputEventKey) and (event.keycode == KEY_ENTER) and event.is_pressed():
 		if menu_visible:
-			create_tween().tween_property($MainMenu/Labels, "modulate", Color(1.0, 1.0, 1.0, 0.0), 0.5)
-			create_tween().tween_property($SceneRoot/Camera3D, "position", Vector3(0, 0, 2.5), 0.5).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+			create_tween().tween_property($MainMenu/Labels, "modulate", Color(1.0, 1.0, 1.0, 0.0), 0.25)
+			create_tween().tween_property($SceneRoot/Camera3D, "position", Vector3(0, 0.5, 5), 0.5).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
 			menu_visible = false
-			interactable = false
-		else:
-			create_tween().tween_property($MainMenu/Labels, "modulate", Color(1.0, 1.0, 1.0, 1.0), 0.5)
-			create_tween().tween_property($SceneRoot/Camera3D, "position", Vector3(-1, 0, 3), 0.5).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
-			menu_visible = true
 			interactable = true
+		else:
+			create_tween().tween_property($MainMenu/Labels, "modulate", Color(1.0, 1.0, 1.0, 1.0), 0.25)
+			create_tween().tween_property($SceneRoot/Camera3D, "position", Vector3(-1.2, 0.5, 6), 0.5).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+			menu_visible = true
+			interactable = false
 
 
 func _on_info_screen_dismiss():
@@ -61,7 +66,8 @@ func _on_info_screen_dismiss():
 	tween.tween_property(current_info_screen, "modulate", Color(1.0, 1.0, 1.0, 0.0), 0.25)
 	tween.tween_callback(func ():
 		current_info_screen.queue_free()
-		interactable = true
+		interactable = false
+		info_screen_open = false
 	)
 
 
@@ -78,3 +84,23 @@ func _on_language_pressed(id: int) -> void:
 			TranslationServer.set_locale("en")
 			$MainMenu/Labels/LanguageButton.text = "EN"
 			
+
+
+func _on_history_button_pressed() -> void:
+	load_info_screen("history")
+
+
+func _on_analysis_button_pressed() -> void:
+	load_info_screen("analysis")
+
+
+func _on_technology_button_pressed() -> void:
+	load_info_screen("technology")
+
+
+func _on_sonaglio_animation_timer_timeout() -> void:
+	if !interactable and !info_screen_open:
+		$SceneRoot/SonaglioAnimationPlayer.play("shake")
+	var time = 10 + (randi() % 6)
+	print(time)
+	$SceneRoot/SonaglioAnimationTimer.start(time)
--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -1,2 +1,3 @@
+extends Node
 
 var current_language = "it"
